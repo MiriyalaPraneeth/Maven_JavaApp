4225e71485e65b73c8321b89a7e811eb6a3b894b
unknown <praneeth333333@gmail.com>
2023-11-21 21:41:14 +0530

    maven java push

---
+++ src/test/java/Praneeth_Miriyala/JavaApp/AppTest.java
@@ -0,0 +1,20 @@
+package Praneeth_Miriyala.JavaApp;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
